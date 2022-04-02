3e1df72b938024902cddd7fc04e2d38bcba67ab6
unknown <Amanullah21>
2022-04-02 23:54:35 +0530

    Basic Structer of Royal Brother Website

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>let start code guys</h1>
+     <h1>Please look readme file first than do the code as well</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/Traiff.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Traiff = () => {
+  return (
+    <div>Traiff</div>
+  )
+}
+
+export default Traiff
