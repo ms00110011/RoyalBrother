23d0bf66408d9f02112528a60964718a1d376107
unknown <Amanullah21>
2022-04-05 20:25:15 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './Page/HomeComponets/Home';
 function App() {
   return (
     <div className="App">
-       <h1>Navbar session</h1>
+       <h1>let start code guys</h1>
+     <h1>Please look readme file first than do the code as well</h1>
       <Home/>
      </div>
   );
