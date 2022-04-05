9a31aaafedfd46476e1b58614a244be3500cb631
unknown <Amanullah21>
2022-04-05 20:17:24 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Home from './Page/HomeComponets/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>let start code guys</h1>
-     <h1>Please look readme file first than do the code as well</h1>
-    </div>
+       <h1>Navbar session</h1>
+      <Home/>
+     </div>
   );
 }
 
---
+++ src/Page/HomeComponets/OurFleet.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./Home.module.css"
+
+const OurFleet = () => {
+  return (
+    <div className={style.fleet_card}>
+        
+    </div>
+  )
+}
+
+export default OurFleet
