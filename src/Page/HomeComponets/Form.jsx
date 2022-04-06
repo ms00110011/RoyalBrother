c23f409730e269a001dc0255e46d6c3a21ac603b
unknown <Amanullah21>
2022-04-06 14:00:22 +0530

    hme components

---
+++ src/Page/HomeComponets/Form.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import style from "./Home.module.css"
+
+const Form = () => {
+    
+//   console.log(today)
+const init = {
+    pickupDate:"",
+    pickupTime:"",
+    dropDate :"",
+    dropTime:""
+}
+const  [data, setData] = useState(init)
+
+const dataHandle = (e) =>{
+    console.log(data)
+// //     console.log(pickupDate);
+// //     const {pickupDate,pickupTime,dropDate,dropTime} = e.target;
+// //     setData({...data, [name]:value})
+}
+// console.log(data)
+  return (
+    <div>
+        <form className={style.landing_form}>
+            <h2>Search your next ride</h2>
+            <p>Pickup</p>
+            <input type="date" name="pickupDate" placeholder='Date' value={data.pickupDate} />
+            <input type="time" name="pickupTime" id="" placeholder='Time' value={data.pickupTime} /> <br />
+            <p>Dropoff</p>
+            <input type="date" name="dropDate"/>
+            <input type="time" name="dropTime"/> <br />
+            <button>Search</button>
+        </form>
+    </div>
+  )
+}
+
+export default Form
