56f3797f4e7f45c19845e588a669862eca91b5b7
Manoj <amnishad0512@gmail.com>
2022-04-07 22:03:48 +0530

    Solved css conflict

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Partner /> */}
+      <Partner />
       {/* <Franchise /> */}
       {/* <Woman /> */}
       {/* <Routes>
