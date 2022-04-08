b407f3d3e281d97e47d9573d58e8dd660510d45a
unknown <Amanullah21>
2022-04-08 16:48:13 +0530

    blog2

--- src/Page/BlogComponents/Blog.jsx
+++ src/Page/BlogComponents/Blog.jsx
@@ -3,6 +3,7 @@ import style from "./Blog.module.css";
 
 import rightArrow from "../HomeComponets/icons/right-arrow.svg";
 import leftArrow from "../HomeComponets/icons/left-arrow.svg";
+import Line from "../HomeComponets/Line";
 
 export const Blog = () => {
   const [data, setData] = useState([]);
@@ -17,34 +18,36 @@ export const Blog = () => {
 
   useEffect(() => getTodo(), []);
   return (
-    <>    <div className={style.blog_container}>
-      {data.map((ele, index) => (
-        <div className={style.card}>
-          <img src={ele.img} alt="" />
-          <div className={style.text_all}>
-            <div className={style.blog_text1}>
-              <spna>{ele.text1}</spna>
-            </div>
-            <div className={style.blog_text2}>
-              <span style={{ color: "black" }}>{ele.text2}</span>
-            </div>
-            <div className={style.blog_text3}>
-              <span>{ele.text3}</span>
+    <>
+      <h1 className={style.blog_h1}>Blog</h1>
+      <Line/>
+      <div className={style.blog_container}>
+        
+        {data.map((ele, index) => (
+          <div className={style.card}>
+            <img src={ele.img} alt="" />
+            <div className={style.text_all}>
+              <div className={style.blog_text1}>
+                <spna>{ele.text1}</spna>
+              </div>
+              <div className={style.blog_text2}>
+                <span style={{ color: "black" }}>{ele.text2}</span>
+              </div>
+              <div className={style.blog_text3}>
+                <span>{ele.text3}</span>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-      
-    </div>
-    <div className={style.arrow_btn}>
+        ))}
+      </div>
+      {/* <div className={style.arrow_btn}>
         <button>
           Left Arrow
         </button>
         <button>
           Right Arrow
         </button>
-      </div>
+      </div> */}
     </>
-
   );
 };
