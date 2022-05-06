e08b546ebc1a5936b977ac90f1d8f5254d8d9c68
Manoj <amnishad0512@gmail.com>
2022-05-06 11:05:43 +0530

    map added

--- src/Page/ReachComponents/Reach.jsx
+++ src/Page/ReachComponents/Reach.jsx
@@ -4,8 +4,10 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import PhoneIcon from "@mui/icons-material/Phone";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import EmailIcon from "@mui/icons-material/Email";
+
 export const Reach = () => {
-    
+  let map =
+    "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15258072.40570261!2d82.75252935!3d20.98801345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1651814882640!5m2!1sen!2sin";
   return (
     <div className={styles.mainContainer}>
       <div className={styles.reachContainer}>
@@ -16,7 +18,9 @@ export const Reach = () => {
               <div className="col-lg-3 col-md-3 col-sm-3 col-3 text-center">
                 <LocationOnIcon />
               </div>
-              <div className={`col-lg-9 col-md-9 col-sm-9 col-9 ${styles.contactDes}`}>
+              <div
+                className={`col-lg-9 col-md-9 col-sm-9 col-9 ${styles.contactDes}`}
+              >
                 Royal Brothers <br /> #501, Bhoomi plaza, 4th Cross Indira Nagar
                 2nd stage, CMH road, Next to Indiranagar metro station
                 Bangalore, Karnataka 560 038
@@ -27,7 +31,9 @@ export const Reach = () => {
               <div className="col-lg-3 col-md-3 col-sm-3 col-3 text-center">
                 <PhoneIcon />
               </div>
-              <div className={`col-lg-9 col-md-9 col-sm-9 col-9 ${styles.contactDes}`}>
+              <div
+                className={`col-lg-9 col-md-9 col-sm-9 col-9 ${styles.contactDes}`}
+              >
                 <a className={styles.link} cl href="tel:+917795687594">
                   +917795687594
                 </a>{" "}
@@ -42,13 +48,23 @@ export const Reach = () => {
               <div className="col-lg-3 col-md-3 col-sm-3 col-3  text-center">
                 <WhatsAppIcon />
               </div>
-              <div className={`col-lg-9 col-md-9 col-sm-9 col-9 ${styles.contactDes} ${styles.link}`}>
+              <div
+                className={`col-lg-9 col-md-9 col-sm-9 col-9 ${styles.contactDes} ${styles.link}`}
+              >
                 <a
                   className={styles.link}
                   href="https://wa.me/919019595595"
                   target="_blank"
                 >
                   Chat now
+                </a>{" "}
+                <span>| </span>
+                <a
+                  className={styles.link}
+                  href="https://wa.me/919019595595"
+                  target="_blank"
+                >
+                  Get QR Code
                 </a>{" "}
               </div>
             </div>
@@ -57,13 +73,17 @@ export const Reach = () => {
               <div className="col-lg-3 col-md-3 col-sm-3 col-3 text-center">
                 <EmailIcon />
               </div>
-              <div className={`col-lg-9 col-md-9 col-sm-9 col-9 ${styles.contactDes}`}>
+              <div
+                className={`col-lg-9 col-md-9 col-sm-9 col-9 ${styles.contactDes}`}
+              >
                 Enquiries - support@royalbrothers.com <br />
                 Grievances - ceo@royalbrothers.com
               </div>
             </div>
           </div>
-          <div className="col-lg-5 col-md-5 col-sm-5 col-12 px-4"></div>
+          <div className="col-lg-5 col-md-5 col-sm-5 col-12 px-4">
+            <iframe src={map} className={styles.map}></iframe>
+          </div>
         </div>
         <div className="row px-4 mb-5">
           <div className="col-lg-4 col-md-4 col-sm-4">
